bff6de0a9e23c3ee2257a2521e11471d6dfac822
timsong-cpp <rs2740@gmail.com>
2016-08-23 17:51:46 -0400

    set this up

---
+++ gen_html.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+cd draft/source
+latexmk -pdf std
+
+cd ../../cxxdraft-htmlgen
+runhaskell genhtml.hs ../draft
+
+rm -r ../gh-pages/*
+cp -r 14882/* ../gh-pages/
+rm -r 14882
+
+cd ../gh-pages
+git add -A
+git commit -m 'Update'
+
+cd ..
