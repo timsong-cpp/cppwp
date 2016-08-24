06100a4685bbf6459b2fd930ad4d8cdaa5bd5dfd
timsong-cpp <rs2740@gmail.com>
2016-08-24 16:11:13 -0400

    Test Bare instead

--- gen_html.sh
+++ gen_html.sh
@@ -6,16 +6,18 @@ cd draft/source
 latexmk -pdf std
 
 cd ../../cxxdraft-htmlgen
-runhaskell genhtml.hs ../draft
+runhaskell genhtml.hs ../draft Bare
 
 rm -r ../gh-pages/*
-cp -r 14882/* ../gh-pages/
+cp -r 14882/math ../gh-pages/
+cd 14882
+find * -maxdepth 0 -type f -exec cp '{}' ../../gh-pages/'{}'.html \;
 rm -r 14882
 
 cd ../gh-pages
 git add -A
 git commit -m 'Update'
-git push
+#git push
 
 cd ..
 git commit -am 'Update'
