ccf52c4712b35a49a817175bce322e21f45dc66d
timsong-cpp <rs2740@gmail.com>
2016-08-24 16:18:51 -0400

    Update

--- gen_html.sh
+++ gen_html.sh
@@ -6,15 +6,17 @@ cd draft/source
 latexmk -pdf std
 
 cd ../../cxxdraft-htmlgen
+rm -rf 14882
 runhaskell genhtml.hs ../draft Bare
 
 rm -r ../gh-pages/*
 cp -r 14882/math ../gh-pages/
-cd 14882
-find * -maxdepth 0 -type f -exec cp '{}' ../../gh-pages/'{}'.html \;
+find 14882/ -maxdepth 1 -type f -execdir cp '{}' ../../gh-pages/'{}'.html \;
 rm -r 14882
 
 cd ../gh-pages
+mv 14882.css.html 14882.css
+mv index.html.html index.html
 git add -A
 git commit -m 'Update'
 #git push
