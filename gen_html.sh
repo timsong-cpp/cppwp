4da116f2d25b1662c495abfb917d3d70c28c72aa
timsong-cpp <rs2740@gmail.com>
2016-11-16 14:53:33 -0500

    Update

--- gen_html.sh
+++ gen_html.sh
@@ -2,6 +2,7 @@
 
 git submodule foreach git pull origin
 
+# Build the standard
 cd draft/source
 latexmk -pdf std
 
@@ -14,10 +15,16 @@ cp -r 14882/math ../gh-pages/
 find 14882/ -maxdepth 1 -type f -execdir cp '{}' ../../gh-pages/'{}'.html \;
 rm -r 14882
 
-cd ../gh-pages
+# Build the networking TS
+cd ../networking-ts/src
+latexmk -pdf ts
+
+# Fixup gh-pages
+cd ../../gh-pages
 mv 14882.css.html 14882.css
 mv index.html.html index.html
 cp ../draft/source/std.pdf ./draft.pdf
+cp ../networking-ts/src/ts.pdf ./networking-ts.pdf
 git add -A
 git commit -m 'Update'
 git push
